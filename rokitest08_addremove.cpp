d4de10bbc1cd98e7e659d4309ae3bd359706022b
yoggy <yoggy0@gmail.com>
2015-12-19 05:38:22 -0800

    ...

--- rokitest08_addremove.cpp
+++ rokitest08_addremove.cpp
@@ -16,95 +16,98 @@ extern "C" {
 	rkFDCell* _rkFDCellPush(rkFD* fd, rkFDCell* lc);
 }
 
-#define DT 0.01
-#define T  10 
+#define DT 0.1
+#define T  8 
 
-#define OBJECT_LIFE_SEC 3 
+#define OBJECT_LIFE_SEC 3
+#define USE_RK_FD_CHAIN_UNREG 1
 
 class RokiObject {
-public:
-	RokiObject(rkFD *fd, const char *name, const byte &joint_type) : t(0) {
-		this->name = name;
+	public:
+		RokiObject(rkFD *fd, const char *name, const byte &joint_type) : t(0) {
+			this->name = name;
 
-		// cell
-		lc = zAlloc(rkFDCell, 1);
+			// cell
+			lc = zAlloc(rkFDCell, 1);
 
-		// chain
-		chain = &(lc->data.chain);
-		rkChainInit(chain);
-		zArrayAlloc(&(chain->link), rkLink, 1);
+			// chain
+			chain = &(lc->data.chain);
+			rkChainInit(chain);
+			zNameSet(chain, const_cast<char*>(name));
+			zArrayAlloc(&(chain->link), rkLink, 1);
 
-		// link
-		rklink = rkChainLink(chain, 0);
-		rkLinkInit(rklink);
+			// link
+			rklink = rkChainLink(chain, 0);
+			rkLinkInit(rklink);
 
-		zVec3DCreate(rkLinkOrgPos(rklink), 0, 0, 0);
-		zMat3DCreate(
-				rkLinkOrgAtt(rklink),
-				1,  0,  0,
-				0,  1,  0,
-				0,  0,  1);
+			zVec3DCreate(rkLinkOrgPos(rklink), 0, 0, 0);
+			zMat3DCreate(
+					rkLinkOrgAtt(rklink),
+					1,  0,  0,
+					0,  1,  0,
+					0,  0,  1);
 
-		// mass
-		rkLinkSetMass(rklink, 1.0);
-		zVec3DCreate(rkLinkCOM(rklink), 0, 0, 0);
-		zMat3DCreate(
-				rkLinkInertia(rklink),
-				0.01,    0,    0,
-				0,    0.01,    0,
-				0,       0, 0.01);
+			// mass
+			rkLinkSetMass(rklink, 1.0);
+			zVec3DCreate(rkLinkCOM(rklink), 0, 0, 0);
+			zMat3DCreate(
+					rkLinkInertia(rklink),
+					0.01,    0,    0,
+					0,    0.01,    0,
+					0,       0, 0.01);
 
-		// joint
-		joint = rkLinkJoint(rklink);
-		rkJointCreate(joint, joint_type);
+			// joint
+			joint = rkLinkJoint(rklink);
+			rkJointCreate(joint, joint_type);
 
-		rkChainSetMass(chain, 1.0);
-		rkChainSetOffset(chain);
-		rkChainUpdateFK(chain);
-		rkChainUpdateID(chain);
-	}
+			rkChainSetMass(chain, 1.0);
+			rkChainSetOffset(chain);
+			rkChainUpdateFK(chain);
+			rkChainUpdateID(chain);
+		}
 
-	void set_pos(const double &x, const double &y, const double &z) {
-		// you must set joint displacements after _rkFDCellPush() calling....
-		zVec dis = zVecAlloc(rkChainJointSize(chain));
-		zVecSetElem(dis, 0, x);
-		zVecSetElem(dis, 1, y);
-		zVecSetElem(dis, 2, z);
-		zVecSetElem(dis, 3, 0);
-		zVecSetElem(dis, 4, 0);
-		zVecSetElem(dis, 5, 0);
-		rkFDChainSetDis(lc, dis);
-		zVecFree(dis);
-	}
+		void set_pos(const double &x, const double &y, const double &z) {
+			// you must set joint displacements after _rkFDCellPush() calling....
+			zVec dis = zVecAlloc(rkChainJointSize(chain));
+			zVecSetElem(dis, 0, x);
+			zVecSetElem(dis, 1, y);
+			zVecSetElem(dis, 2, z);
+			zVecSetElem(dis, 3, 0);
+			zVecSetElem(dis, 4, 0);
+			zVecSetElem(dis, 5, 0);
+			rkFDChainSetDis(lc, dis);
+			zVecFree(dis);
+		}
 
-	double get_pos(const int &idx) const {
-		zVec dis;
-		dis = zVecAlloc(rkChainJointSize(chain));
-		rkChainGetJointDisAll(chain, dis);
-		double val = zVecElem(dis, idx);
-		zVecFree(dis);
-		return val;
-	}
+		double get_pos(const int &idx) const {
+			zVec dis;
+			dis = zVecAlloc(rkChainJointSize(chain));
+			rkChainGetJointDisAll(chain, dis);
+			double val = zVecElem(dis, idx);
+			zVecFree(dis);
+			return val;
+		}
 
-	double x() const {
-		return get_pos(0);
-	}
+		double x() const {
+			return get_pos(0);
+		}
 
-	double y() const {
-		return get_pos(1);
-	}
+		double y() const {
+			return get_pos(1);
+		}
 
-	double z() const {
-		return get_pos(2);
-	}
-public:
-	rkFDCell *lc;
-	rkChain  *chain;
-	rkLink   *rklink;
-	rkJoint  *joint;
+		double z() const {
+			return get_pos(2);
+		}
 
-	std::string name;
-	double t;
+	public:
+		rkFDCell *lc;
+		rkChain  *chain;
+		rkLink   *rklink;
+		rkJoint  *joint;
+
+		std::string name;
+		double t;
 };
 
 int main(int argc, char *argv[])
@@ -129,13 +132,32 @@ int main(int argc, char *argv[])
 	rkFDSetSolver(&fd, Volume);
 	rkFDUpdateInit(&fd);
 
-	// setup ncurses
+	// init ncurses screen
 	initscr();
 
 	// main loop
 	unsigned int append_count = 0;
 	while (rkFDTime(&fd) < T){
-		// append 
+#if USE_RK_FD_CHAIN_UNREG
+		std::vector<RokiObject*>::iterator it = objs.begin();
+
+		while(it != objs.end()) {
+			if ((*it)->t > OBJECT_LIFE_SEC) {
+				rkFDCell *lc = (*it)->lc;
+				rkFDChainUnreg(&fd, lc);
+
+				rkFDUpdateDestroy(&fd);
+				rkFDUpdateInit(&fd);
+
+				delete *it;          
+				it = objs.erase(it); 
+			}
+			else {
+				++it;
+			}
+		}
+#endif
+		// push rkFDCell
 		if ((unsigned int)(rkFDTime(&fd)) > append_count) {
 			std::stringstream ss;
 			ss << "obj" << append_count;
@@ -146,19 +168,8 @@ int main(int argc, char *argv[])
 			objs.push_back(obj); 
 			append_count ++;
 
+			rkFDUpdateDestroy(&fd);
 			rkFDUpdateInit(&fd); // ループの途中で追加・削除する場合は、rkFDUpdateInit()を呼ぶこと
-		}
-
-		// remove
-		std::vector<RokiObject*>::iterator it = objs.begin();
-		while(it != objs.end()) {
-			if ((*it)->t > OBJECT_LIFE_SEC) {
-				delete *it;          // delete RokiObject instance
-				it = objs.erase(it); // erase pointer from vector
-			}
-			else {
-				++it;
-			}
 		}
 
 		// update forward dynamics
@@ -177,13 +188,12 @@ int main(int argc, char *argv[])
 			double y = objs[i]->y();
 			double z = objs[i]->z();
 
-			mvprintw(i, 15, "name=%s, pos=(%f, %f, %f)", objs[i]->name.c_str(), x, y, z);
+			//printf("name=%s, pos=(%f, %f, %f), addr=%x\n", objs[i]->name.c_str(), x, y, z, objs[i]->chain);
+			mvprintw(i, 15, "name=%s, pos=(%f, %f, %f), addr=%x", objs[i]->name.c_str(), x, y, z, objs[i]->chain);
 			mvprintw((int)(11-z), x + 10, "*");  // cube
 		}
-		
+
 		refresh();
-		
-		//refresh();
 		usleep((int)(DT * 1000 * 1000));
 	}
 
